5ced99e71aab4668fda3121d517b97d0c493af67
Estifo <135218620+EstifanosChekaFaltamo@users.noreply.github.com>
2024-09-28 00:33:20 +0300

    amazon-api

---
+++ index.js
@@ -0,0 +1,56 @@
+
+
+
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+// const { Message } = require("firebase-functions/v2/pubsub");
+const stripe = require("stripe")(process.env.STRIPE_KEY)
+
+const app = express();
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        Message: "Sucess!",
+    });
+});
+
+app.post("/payment/create", async (req, res) => {
+    const total = req.query.total;
+    if (total > 0) {
+        // console.log("payment recieved", total);
+        // res.send(total)
+
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: total,
+            currency: "usd"
+        })
+        res.status(201).json({
+            clientSecret: paymentIntent.client_secret,
+        })
+    } else {
+        res.status(403).json({
+            Message: "Total must be greater than 0",
+        });
+    }
+})
+
+
+
+app.listen(5000, (err)=>{
+    if(err) throw err 
+    console.log("Amazon Server Running on PORT:5000, http://localhost:5000");
+})
+
+// exports.api = onRequest(app);
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
